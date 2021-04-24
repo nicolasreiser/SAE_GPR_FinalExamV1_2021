7b952fc1dd8ca454d203d0e8ce06af4936cc6a73
nicolasreiser <nico.reiser@gmail.com>
2021-04-24 10:35:10 +0200

    Task 1: Bugfix completed
    Isuue: Rare soul drop name color was black instead of blue.
    The color in the class DropRarityExtentions was not set up correctly. The color for rare items was set to black instead of blue.

--- Assets/Scripts/Loot/Drop.cs
+++ Assets/Scripts/Loot/Drop.cs
@@ -35,7 +35,7 @@ public static class DropRarityExtensions
                 return Color.white;
 
             case DropRarity.Rare:
-                return Color.black;
+                return Color.blue;
 
             case DropRarity.Epic:
                 return new Color(1, 0, 1, 1);
