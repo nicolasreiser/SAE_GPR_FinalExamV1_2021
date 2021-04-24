35c35498abc4e72aed42d468754e9cca7ca9199b
nicolasreiser <nico.reiser@gmail.com>
2021-04-24 11:55:42 +0200

    Task 5: Iterate 2 completed
    Task: add an outline to the ability when charging and scale it up, scale it back down when on cooldown.
    Added an outline module to the icon that gets enable when the ability is getting charged and gets disabled when finished charging. a coroutine get sused to scale the ability UI

--- Assets/Scripts/UI/PlayerHud.cs
+++ Assets/Scripts/UI/PlayerHud.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHud : MonoBehaviour
 {
     [SerializeField] private SpellCastingController spellCastingController;
     [SerializeField] private DropCollector dropCollector;
 
+    [SerializeField] private GameObject abilityCooldownPackage;
     [SerializeField] private Image spellIcon;
     [SerializeField] private TMPro.TMP_Text spellCooldownText;
     [SerializeField] private GameObject collectUIObject;
+    [SerializeField] private Outline spellOutline;
+    [SerializeField] private float scaleSpeed;
+    [SerializeField] private float transitionTime;
+
+    private Vector3 originalAbilityScale;
+    private float remainingTransitionTime;
+    private bool transitionCoroutine;
 
     private void Start()
     {
@@ -16,6 +25,9 @@ public class PlayerHud : MonoBehaviour
         Debug.Assert(dropCollector != null, "DropCollector reference is null");
 
         spellIcon.sprite = spellCastingController.SimpleAttackSpellDescription.SpellIcon;
+        spellOutline.enabled = false;
+        originalAbilityScale = abilityCooldownPackage.transform.localScale;
+        transitionCoroutine = false;
 
         dropCollector.DropsInRangeChanged += OnDropsInRangeChanged;
     }
@@ -38,5 +50,48 @@ public class PlayerHud : MonoBehaviour
             spellCooldownText.text = "";
             spellIcon.color = Color.white;
         }
+
+        if(spellCastingController.IsInAction())
+        {
+            spellOutline.enabled = true;
+            scaleCastedAbility();
+        }
+        if(!spellCastingController.IsInAction() && cooldown > 0)
+        {
+            spellOutline.enabled = false;
+            Resetscale();
+        }
+    }
+
+    private void scaleCastedAbility()
+    {
+        Vector3 scale = abilityCooldownPackage.transform.localScale;
+
+        scale += Vector3.one * Time.deltaTime * scaleSpeed;
+
+        abilityCooldownPackage.transform.localScale = scale;
+    }
+    private void Resetscale()
+    {
+        if(!transitionCoroutine)
+        {
+            transitionCoroutine = true;
+            StartCoroutine(TransitionTimeCoroutine());
+        }
+    }
+
+    IEnumerator TransitionTimeCoroutine()
+    {
+        float currentDistance = 0;
+        Vector3 startScale = abilityCooldownPackage.transform.localScale;
+        while (currentDistance <= transitionTime)
+        {
+            float fractionOfDistance = currentDistance / transitionTime;
+            abilityCooldownPackage.transform.localScale = Vector3.Lerp(startScale, originalAbilityScale,fractionOfDistance);
+
+            currentDistance += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        transitionCoroutine = false;
     }
 }
